40765d897d793ed0829623438f2bb37afd42e217
Shah Jalal <sjalal0228@sjalal0228-VirtualBox.(none)>
2013-06-26 11:57:28 -0500

    modified some layout

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# if Rails.env.development? or Rails.env.test?
+#    GooglePlacesAutocomplete::Application.config.secret_token = '98eb15ae0b2e1077c3f0f8b620e1cfc7d49f7336bde659166939157576bbf792649b87355050f49e5d1fc1aee40b2b40ef784748a841c25a52ba5ce3708b2263'
+# else 
+# 	GooglePlacesAutocomplete::Application.config.secret_token = ENV['SECRET_TOKEN']
+# end 
+
 GooglePlacesAutocomplete::Application.config.secret_token = '98eb15ae0b2e1077c3f0f8b620e1cfc7d49f7336bde659166939157576bbf792649b87355050f49e5d1fc1aee40b2b40ef784748a841c25a52ba5ce3708b2263'
